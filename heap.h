fc93ed690c1f44d64d2ad7ddfd8132f3ddf41d97
ilikecheez5056 <btse7482@sdsu.edu>
2025-10-24 14:07:00 +0100

    Initial Commit

--- heap.h
+++ heap.h
@@ -16,6 +16,9 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        data[size] = idx;
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
